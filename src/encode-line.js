f2bf9798180f67a0199d87095c8f525cccddbb5b
FavelLooking <intwestpol@gmail.com>
2023-12-17 03:02:21 +0400

    feat: complete extended-repeater task

--- src/encode-line.js
+++ src/encode-line.js
@@ -14,28 +14,18 @@ function encodeLine(str) {
   if (str.length === 0) {
     return '';
   }
-  console.debug('str: ' + str)
   let arr = str.split('');
-  console.debug('arr: ' + arr)
   let count = 0;
   let result = 0;
   for (let i = 0; i <arr.length; i += 1) {
     if (arr[i] === arr[i+1]) {
-      console.debug('yes')
-      console.debug('\\/')
-      console.debug('i: ' + i)
       count += 1;
-      console.debug('result 1 loop: ' + result)
     } else { count ++;
       result += count;
       count = 0
-      console.debug('no')
       result+= arr[i]
-      console.debug('result 2 loop: ' + typeof result)
     }
   }
-  console.debug(result)
-  console.debug('----------------------' +typeof result)
   return result.split('').filter((a)=> a!=='1').join('');
 }
 
--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,9 +15,65 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  console.debug(`str: ${str}, options: ${options}`)
+  let result = '';
+  let {repeatTimes, separator, addition, additionRepeatTimes, additionSeparator} = options
+  console.debug(`
+                repeatTimes: ${repeatTimes}, 
+                separator: ${separator}, 
+                addition: ${addition}, 
+                additionRepeatTimes: ${additionRepeatTimes}, 
+                additionSeparator: ${additionSeparator}
+  `)
+  if (!Boolean(repeatTimes)) {
+    repeatTimes = 1;
+  }
+  if (!Boolean(additionRepeatTimes)) {
+    additionRepeatTimes = 1;
+  }
+  if (typeof addition === 'boolean' || typeof addition === 'object') {
+    addition = String(addition)
+  }
+  
+  if (repeatTimes) {
+    for (let i = 0; i <repeatTimes; i +=1) {
+      result +=String(str);
+      if (Boolean(addition)) {
+        if (!!additionSeparator) {
+          for (let i = 0; i <additionRepeatTimes; i +=1) {
+            result +=String(addition)
+            if (i === additionRepeatTimes - 1){
+              break;
+            }
+            result +=String(additionSeparator)
+          }
+        } else if (!additionSeparator && additionRepeatTimes > 1)
+        ////////////////////////////////
+        for (let i = 0; i <additionRepeatTimes; i +=1) {
+          result +=String(addition);
+        if (i === additionRepeatTimes - 1){
+          break;
+        }
+          result += '|'
+        }
+        ////////////////////////////////
+        else result +=String(addition);
+
+      }
+      if (i === repeatTimes - 1){
+        break;
+      }
+      
+      if (!!separator) {
+        result +=separator;
+      } 
+      else
+  
+      result += '+'
+    }
+  }
+  return result;
 }
 
 module.exports = {
